d7b2b499733ece16894081f36c1ff6c4ecf676f6
christianvandermeer <129622289+christianvandermeer@users.noreply.github.com>
2023-04-02 12:53:45 -0400

    Full Sensor Implementation + Handled Cases for temperature

--- smarthomeBoard/SensorDataRetrieval.cpp
+++ smarthomeBoard/SensorDataRetrieval.cpp
@@ -1,22 +1,61 @@
 #include "SensorDataRetrieval.h"
 
-//**NOTE**:
-/* This function works for simply recieving temperature in C and humidity %
-(This is the only sensor attatched to this arduino) */
-
-int getSensorDataa(deviceType d) {
+int getSensorData(deviceType d) {
+  int roomTemperature;
+  int humidity;
+  int photoresistor;
+  
   DHT dht(DHTPIN, DHTTYPE);
 
-  //Sent value was for TEMP
-  if(d==temp) {
-    int roomTemperature = dht.readTemperature();
+  //Send request was for TEMP
+  if(d==temp) 
+  {
+  roomTemperature = dht.readTemperature();
+  if(isnan(humidity) || isnan(roomTemperature)){
+    //DHT not connected, so instead just grab a temp from thermistor
+    roomTemperature = THERM();
+  }
+
     Serial.println(roomTemperature);
     return roomTemperature;
   }
-  //Sent value was for HUMIDITY
-  if(d==humid) {
-    int humidity = dht.readHumidity();
+  //Send request was for HUMIDITY
+  if(d==humid) 
+  {
+    humidity = dht.readHumidity();
     return humidity;
   }
+//Send request was for PHOTORESISTANCE
+if(d==photo)
+{
+  photoresistor = PHOTO();
+  return photoresistor;
+
+}
+}
+
+//CALCULATION FUNCTIONS
+int THERM(){  //Function to calculate thermistor temperature
+  int Vo;
+  float R1 = 10000;
+  float logR2, R2, tKelvin, tCelsius;
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+  Vo = analogRead(thermPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0); 
+  logR2 = log(R2);
+  tKelvin = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+  tCelsius = tKelvin - 273.15;
+  return tCelsius;
+}
+
+int PHOTO(){  //Function to calculate photoresistance in lux
+int resistance = 10000;
+int readDelay = 1000;
+int reading = analogRead(photoPin);
+float voltage = reading * 5.0 / 1024.0;
+float Rphoto = 5.0 / voltage * resistance - resistance;
+float lightIntensity = 500/(Rphoto/1000);
+delay(readDelay);
+return lightIntensity;
 }
 
--- smarthomeBoard/SensorDataRetrieval.h
+++ smarthomeBoard/SensorDataRetrieval.h
@@ -4,9 +4,14 @@
 #include "transmission.h"
 #include "DHT.h"
 #define DHTTYPE DHT11   // DHT 11
-#define DHTPIN 2  // define the digital pin the DHT is connected to
+#define DHTPIN 2     // define the digital pin the DHT is connected to
+#define thermPin A0  // define the analog pin the thermistor is connected to
+#define photoPin A1  // define the analog pin the photoresistor is connected to
+
 #include <Arduino.h>
 
-int getSensorDataa(deviceType d);
+int getSensorData(deviceType d);
+int THERM();
+int PHOTO();
 
 #endif
