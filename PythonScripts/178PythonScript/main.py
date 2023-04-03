925c0768c391528e4765da142403430afb0965c0
ImraanParthab <116522998+ImraanParthab@users.noreply.github.com>
2023-04-03 02:14:00 -0400

    Python print out working better. v.2.0
    This was a pain. i dont want to code.
    
    prints serial to the output. makes web page that updates.

--- PythonScripts/178PythonScript/main.py
+++ PythonScripts/178PythonScript/main.py
@@ -1,9 +1,10 @@
 import serial
-import datetime
+import serial.tools.list_ports as list_port
 import time as timeLib
 from pandas import DataFrame
 import openpyxl
 
+from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.subplots as make_subplots
@@ -11,88 +12,135 @@ import plotly.subplots as make_subplots
 
 fileLocation = "C:\\Users\\imraa\\OneDrive\\Documents\\GitHub\\ArduinoSmarthome\\PythonScripts\\178PythonScript\\dataOut.xlsx"
 
+class IndivBoard:
+
+    def __init__(self, boardNum):
+        self.boardNum = boardNum
+        self.sensors = []
+        for i in range(1,5):
+            a = IndivSensor(str(i))
+            self.sensors.append(a)
+
+    def addData(self, senType, data):
+        for sensor in self.sensors:
+            if senType == sensor.senType:
+                sensor.updateSelf(data)
+
 class IndivSensor:
 
-    def __init__(self):
+    def __init__(self, senType):
         self.data=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         x = timeLib.time()
         self.time = [x,x,x,x,x,x,x,x,x,x]
-        self.number = ""
-        self.type = '0'
+        self.senType = senType
+        self.updated = False
         self.df = DataFrame(data=self.data, index=self.time)
 
-    def printInfo(self):
-        print(" ----------------- " + self.number + self.type + '---------------')
+    def updateSelf(self, data):
+        self.data.pop(0)
+        self.data.append(data)
+        self.time.pop(0)
+        # time = str(datetime.datetime.now().strftime('%H:%M:%S.%f'))
+        self.time.append(timeLib.time())
+        self.updated = True
+        self.df = DataFrame(data=self.data, index=self.time)
+        # self.printData()
+
+    def printData(self):
+        print("---  " + self.senType + "   ---   " + str(self.data[-1]) + " - " + str(self.time[-1]))
         print(self.df)
 
-    def updataSelf(self):
-        self.time.pop(0)
-        self.time.append(timeLib.time())
-        self.df = DataFrame(data=self.data, index=self.time)
 
-def main():
-    serialInst = serial.Serial()
-    serialInst.baudrate = 9600
-    serialInst.port = 'COM4'
-    serialInst.open()
+# --------------------------------------------------------------------------------------------------------
 
-    var = 0
 
-    # dataOut = openpyxl.load_workbook(fileLocation)
+serialInst = serial.Serial()
+serialInst.baudrate = 9600
+serialInst.port = 'COM4'
+serialInst.open()
 
-    data = []
-    while not serialInst.in_waiting:
-        timeLib.sleep(0.001)
 
-    package = serialInst.readline()
 
-    while True:
-        if serialInst.in_waiting:
-            package = serialInst.readline()
-            line = package.decode('utf')
-            time = str(datetime.datetime.now().strftime('%H:%M:%S.%f'))
+# dataOut = openpyxl.load_workbook(fileLocation)
 
-            ID = line[:3]
+# first index is the board #
+# second index is the sensor type
+boards = []
+while not serialInst.in_waiting:
+    timeLib.sleep(0.001)
 
-            try:
-                numData = int(line[4:])
-            except:
-                numData = 0
-                print("Data is bad")
+package = serialInst.readline()
 
-#            found = False
-#            for i in dataOut.worksheets:
-#                if ID == 1:
-#                    found = True
-#            if not found:
-#                dataOut.create_sheet(ID)
-#                dataOut.save(fileLocation)
+graphs = make_subplots.make_subplots(rows=4, cols=1)
 
+
+app = Dash(__name__, update_title=None)
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+            Graph Sensor: Photo
+        '''),
+
+        dcc.Graph(
+            id='graph1',
+            figure=graphs
+        ),
+    dcc.Interval(
+        id="sensor_update",
+        interval=3000,
+        n_intervals=0
+    )
+])
+
+
+@app.callback(Output('graph1', 'figure'),
+              Input('sensor_update', 'n_intervals'))
+def sensorLoop(n):
+    if serialInst.in_waiting:
+
+        good = True
+        package = serialInst.readline()
+        line = package.decode('utf')
+        print(line)
+
+        try:
+            numData = int(line[4:])
+        except:
+            numData = 0
+            good = False
+            print("Data is bad")
+
+        try:
+            ID = line[:2]
+            sensor = line[2]
+        except:
+            ID = '00'
+            sensor = '0'
+            good = False
+            print("Data is bad")
+
+        if good:
             found = False
-            for i in data:
-                if ID[:2] == i.number and ID[2] == i.type:
-                    i.data.pop(0)
-                    i.data.append(numData)
-                    i.number = line[:2]
-                    i.type = line[2]
-                    i.updataSelf()
-                    #i.printInfo()
+            for board in boards:
+                if ID == board.boardNum:
+                    board.addData(sensor, numData)
                     found = True
+
             if not found:
-                data.append(IndivSensor())
-                data[-1].data.pop(0)
-                data[-1].data.append(numData)
-                data[-1].number = line[:2]
-                data[-1].type = line[2]
-                data[-1].updataSelf()
-                #data[-1].printInfo()
+                boards.append(IndivBoard(ID))
+                boards[-1].addData(sensor, numData)
 
-            fig = make_subplots.make_subplots(rows=1, cols=1)
-            for i in data:
-                fig.add_trace(go.Scatter(x=i.time, y=i.data))
-                var= var+1
-            fig.show()
+        graphs = make_subplots.make_subplots(rows=4, cols=1)
+
+        for board in boards:
+            for j in range(1,5):
+                if board.sensors[j-1].updated:
+                    graphs.add_trace(row=j,col=1,
+                                     trace=go.Scatter(x=board.sensors[j-1].time,
+                                                      y=board.sensors[j-1].data))
+        # graphs.show()
+
+        return graphs
 
 
-
-main()
+app.run_server(debug=False)
