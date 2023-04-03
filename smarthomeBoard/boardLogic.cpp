6c17489facd27bf35bf01f769aa1d17e348acf49
christianvandermeer <129622289+christianvandermeer@users.noreply.github.com>
2023-04-03 00:02:30 -0400

    Handle Actuating cases based on sensor values

--- smarthomeBoard/boardLogic.cpp
+++ smarthomeBoard/boardLogic.cpp
@@ -1,13 +1,14 @@
 #include "boardLogic.h"
 #include "SensorDataRetrieval.h"
+#include "actuatingCases.h"
 #include "actuateMotors.h"
 
 extern SoftwareSerial HC12;
 
-int reciveMessageToQueue(pQueue queue){
+int reciveMessageToQueue(pQueue queue) {
 
   //Check that there is enough data for a message to be possible
-  if(HC12.available() < MINMESSAGELEN) return RETURN_ERR;
+  if (HC12.available() < MINMESSAGELEN) return RETURN_ERR;
 
   //delay to allow new HC12 data to come in
   delay(100);
@@ -15,22 +16,21 @@ int reciveMessageToQueue(pQueue queue){
   Message theMessage = reciveTransmission();
 
   //Check if message is valid (not corrupted)
-  if(!checkMessageValidity(theMessage)) {
+  if (!checkMessageValidity(theMessage)) {
 
     //If valid add to queue
     Enqueue(queue, CreateItem(theMessage));
     return RETURN_OK;
   }
 
-  if(DEBUG) Serial.println("RECIVEDINVALIDMESSAGE");
-  if(DEBUG) Serial.println(checkMessageValidity(theMessage));
+  if (DEBUG) Serial.println("RECIVEDINVALIDMESSAGE");
+  if (DEBUG) Serial.println(checkMessageValidity(theMessage));
   return RETURN_ERR;
-
 }
 
-void dealWithMessage(Message message){
+void dealWithMessage(Message message) {
 
-  if(DEBUG){
+  if (DEBUG) {
     Serial.print("message.messageType ");
     Serial.println(message.messageType);
     Serial.print("message.productNum ");
@@ -41,11 +41,11 @@ void dealWithMessage(Message message){
     Serial.println(message.sensor);
   }
 
-  switch(PRODUCTWHAT){
+  switch (PRODUCTWHAT) {
     case sensorBoard:
-      if(message.messageType == request){ //Check if Message is a request
-        Request theRequest = parseRequest(message); //Parse Message to find 
-        
+      if (message.messageType == request) {          //Check if Message is a request
+        Request theRequest = parseRequest(message);  //Parse Message to find
+
         //string to match format of Request.destination
         char whoIAm[4];
         whoIAm[0] = PRODUCTWHAT;
@@ -53,22 +53,22 @@ void dealWithMessage(Message message){
         whoIAm[2] = PRODUCTNUM[1];
         whoIAm[3] = '\0';
 
-        if(DEBUG) {
+        if (DEBUG) {
           Serial.print("whoIAm: ");
           Serial.println(whoIAm);
           Serial.print("destination ");
           Serial.println(theRequest.destination);
         }
 
-        if(!strcmp(theRequest.destination, whoIAm)){ //Check if I am being requested
-          sendMessage(createMessage(theRequest.device, pureData, getSensorData(theRequest.device))); 
+        if (!strcmp(theRequest.destination, whoIAm)) {  //Check if I am being requested
+          sendMessage(createMessage(theRequest.device, pureData, getSensorData(theRequest.device)));
         }
       }
       break;
 
     case actuatorBoard:
-    if(message.messageType == command){ //Check if Message is a request
-        Request theRequest = parseRequest(message); //Parse Message to find 
+      if (message.messageType == command) {          //Check if Message is a request
+        Request theRequest = parseRequest(message);  //Parse Message to find
 
         //string to match format of Request.destination
         char whoIAm[4];
@@ -77,15 +77,22 @@ void dealWithMessage(Message message){
         whoIAm[2] = PRODUCTNUM[1];
         whoIAm[3] = '\0';
 
-        if(DEBUG) {
+        if (DEBUG) {
           Serial.print("whoIAm: ");
           Serial.println(whoIAm);
           Serial.print("destination ");
           Serial.println(theRequest.destination);
         }
 
-        if(!strcmp(theRequest.destination, whoIAm)){ 
-          // actuateMotors(theRequest.device);
+        if (!strcmp(theRequest.destination, whoIAm)) {
+          if (message.messageType == pot) {
+            actuateMotors(message.data.data.intData);
+          }
+          if(message.messageType==photo){
+            if(message.data.data.intData==1){
+              turnOnLEDs();
+            }
+          }          
         }
       }
 
@@ -97,7 +104,7 @@ void dealWithMessage(Message message){
 
       //printMessageToSerialDEBUG(message);
       outputToSerialInPythonFormat(message);
-
+      sendMessage(createMessage(message.sensor, command, actuatingCases(message)));
 
       break;
 
@@ -105,63 +112,61 @@ void dealWithMessage(Message message){
 
       break;
   }
-
 }
 
-void printMessageToSerialDEBUG(Message message){
+void printMessageToSerialDEBUG(Message message) {
   if (!DEBUG) return;
 
-    Serial.println("message.productWhat");
-    Serial.write(message.productWhat);
-     Serial.println("message.productNum");
-    Serial.write(message.productNum);
-     Serial.println("message.sensor");
-    Serial.write(message.sensor);
-     Serial.println("message.messageType");
-    Serial.write(message.messageType);
-     Serial.println("message.checkbyte");
-    Serial.write(message.checkbyte);
+  Serial.println("message.productWhat");
+  Serial.write(message.productWhat);
+  Serial.println("message.productNum");
+  Serial.write(message.productNum);
+  Serial.println("message.sensor");
+  Serial.write(message.sensor);
+  Serial.println("message.messageType");
+  Serial.write(message.messageType);
+  Serial.println("message.checkbyte");
+  Serial.write(message.checkbyte);
 
-     Serial.println("message.data.type");
-    Serial.write(message.data.type);
+  Serial.println("message.data.type");
+  Serial.write(message.data.type);
 
-    //Switch case prints data in the correct format
-    switch(message.data.type){
-        case intType:
-         Serial.println("message.data.data.intData");
-            Serial.print(message.data.data.intData);
-        break;
+  //Switch case prints data in the correct format
+  switch (message.data.type) {
+    case intType:
+      Serial.println("message.data.data.intData");
+      Serial.print(message.data.data.intData);
+      break;
 
-        case floatType:
-         Serial.println("message.data.data.floatData");
-            Serial.print(message.data.data.floatData);
-        break;
+    case floatType:
+      Serial.println("message.data.data.floatData");
+      Serial.print(message.data.data.floatData);
+      break;
 
-        case strType:
-         Serial.println("message.data.data.strData");
-            Serial.print(message.data.data.strData);
-        break;
+    case strType:
+      Serial.println("message.data.data.strData");
+      Serial.print(message.data.data.strData);
+      break;
 
-        default:
-            //Error case.
-        break;
-    }
- Serial.println("message.endChar");
-    Serial.write(message.endChar);
+    default:
+      //Error case.
+      break;
+  }
+  Serial.println("message.endChar");
+  Serial.write(message.endChar);
 }
 
 
 
-void printRequestToSerialDEBUG(Request request){
+void printRequestToSerialDEBUG(Request request) {
   if (!DEBUG) return;
 
   Serial.println("request.destination");
   Serial.write(request.destination);
-   Serial.println("request.device");
+  Serial.println("request.device");
   Serial.write(request.device);
-   Serial.println("request.type");
+  Serial.println("request.type");
   Serial.write(request.type);
-   Serial.println("request.additional");
-  Serial.print((int) request.additional);
-
+  Serial.println("request.additional");
+  Serial.print((int)request.additional);
 }
