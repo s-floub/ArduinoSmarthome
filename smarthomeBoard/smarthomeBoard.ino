172bc67af47e5130a30720bec0cb3e24908a27ed
christianvandermeer <129622289+christianvandermeer@users.noreply.github.com>
2023-04-03 00:05:26 -0400

    Spacing issues

--- smarthomeBoard/smarthomeBoard.ino
+++ smarthomeBoard/smarthomeBoard.ino
@@ -20,8 +20,8 @@ pQueue errorQueue = CreateQueue();
 pList devicesList = initialiseList();
 
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  HC12.begin(9600);               // Serial port to HC12
+  Serial.begin(9600);  // Serial port to computer
+  HC12.begin(9600);    // Serial port to HC12
 
   pinMode(POTPIN, INPUT);
   pinMode(HC12SETPIN, OUTPUT);
@@ -32,12 +32,10 @@ void setup() {
 
   changeChannel(DEFAULTCHANNEL);
 
-  if (PRODUCTWHAT == mainBoard){
-  insertNode(devicesList, sensorBoard, "11", humid);
-  insertNode(devicesList, sensorBoard, "11", temp);
-
+  if (PRODUCTWHAT == mainBoard) {
+    insertNode(devicesList, sensorBoard, "11", humid);
+    insertNode(devicesList, sensorBoard, "11", temp);
   }
-
 }
 
 char readBuffer[100];
@@ -46,15 +44,13 @@ int adjust = 0;
 int runOnce = 0;
 
 void loop() {
-
-
-  if(PRINTEVERYTHING && DEBUG){
-  while (HC12.available()) {        // If HC-12 has data
+  if (PRINTEVERYTHING && DEBUG) {
+    while (HC12.available()) {  // If HC-12 has data
       Serial.write(HC12.read());
-      };      // Send the data to Serial 
-    }
+    };  // Send the data to Serial
+  }
 
-  else if (MAINBOARD){
+  else if (MAINBOARD) {
 
     queryList(devicesList, messageQueue);
 
@@ -62,20 +58,18 @@ void loop() {
 
   }
 
-  else{
+  else {
     delay(1000);
 
-     if(HC12.available() > MINMESSAGELEN){
-     delay(1000);
-     reciveMessageToQueue(messageQueue);
-      }
+    if (HC12.available() > MINMESSAGELEN) {
+      delay(1000);
+      reciveMessageToQueue(messageQueue);
+    }
 
-      if(messageQueue->count > 0){
+    if (messageQueue->count > 0) {
       Message theMessage;
       Dequeue(messageQueue, theMessage);
       dealWithMessage(theMessage);
-      }
-
+    }
   }
 }
-    
